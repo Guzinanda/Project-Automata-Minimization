52c7d3f5e408862d47fdb2b8f6161c9c562f1b09
Fernanda Guzman Gomez <fernandamac@Fernandas-MacBook-Pro.local>
2020-11-21 19:59:49 -0600

    02. Commit

--- afnd_afd_sinvacios.py
+++ afnd_afd_sinvacios.py
@@ -2,32 +2,56 @@
 CONVERTIDOR DE AFND a AFD 
 
 Input:
-nfa = {'A'   : { '1':['A','B'],         '0':['A']}, 
-       'B'   : { '1':['A','B'],         '0':['A','B','C']}, 
-       'C'   : { '1':['A','B','D'],     '0':['A','B','C']}, 
-       'D'   : { '1':['A','B'],         '0':['A','B','C']}}
+nfa = {'A'   : { '1':['A','B'],       '0':['A']}, 
+       'B'   : { '1':['A','B'],       '0':['A','B','C']}, 
+       'C'   : { '1':['A','B','D'],   '0':['A','B','C']}, 
+       'D'   : { '1':['A','B'],       '0':['A','B','C']}}
 
 Output:
-dfa = {'A'   : { '1':['AB'],    '0':['A']}, 
-       'AB'  : { '1':['AB'],    '0':['ABC']}, 
-       'ABC' : { '1':['ABD'],   '0':['ABC']}, 
-       'ABD' : { '1':['AB'],    '0':['ABC']}}
-
+dfa = {'A'   : { '1':['AB'],          '0':['A']}, 
+       'AB'  : { '1':['AB'],          '0':['ABC']}, 
+       'ABC' : { '1':['ABD'],         '0':['ABC']}, 
+       'ABD' : { '1':['AB'],          '0':['ABC']}}
 '''
+
+
+
 
 
 #? El usuario nos da la tabla del NFA sin E y sin vacios _______________________________________________
 
+'''
 nfa = {'A':{'1':['A','B'],         '0':['A']}, 
        'B':{'1':['A','B'],         '0':['A','B','C']}, 
        'C':{'1':['A','B','D'],     '0':['A','B','C']}, 
        'D':{'1':['A','B'],         '0':['A','B','C']}}
+'''
+
+nfa = {'A':{'1':['B','C'],         '0':['E']}, 
+       'B':{'1':['A'],             '0':['']}, 
+       'C':{'1':['D'],             '0':['']}, 
+       'D':{'1':[''],              '0':['A']},
+       'E':{'1':[''],              '0':['']}}
+
+dfa = {}
+
+'''
+Output:
+dfa = {'A'   : { '1':['BC'],          '0':['E']}, 
+       'BC'  : { '1':['AD'],          '0':['E']}, 
+       'AD'  : { '1':['BC'],          '0':['AE']}, 
+       'E'   : { '1':['N'],           '0':['N']}, 
+       'AE'  : { '1':['BC'],          '0':['E']}}
+'''
+
+
+
+
 
 # Estado inicial del NFA:
 estado_inicial = 'A'
 # Estado final del NFA:
 estado_aceptacion = 'D'  
-
 
 
 #? Algoritmo para encontrar trandisiones de estados ____________________________________________________
@@ -56,8 +80,37 @@ romper = bool
 #    estados_afd = ['A','AB','ABC','ABD']
 #                                   ---
 
+
+#? Algoritmo para remover las transiciones Epsilon del autómata ________________________________________
+#Esto valida las transiciones vacías (inexistentes, no confundir con transiciones Epsilon)
+def quitar_vacios():
+       adelita_sensual = 0
+       for estados in nfa.keys():
+              for transiciones in nfa[estados].keys():
+                     if nfa[estados][transiciones] == ['']:
+                            adelita_sensual = 1 
+                            nfa[estados][transiciones] = 'N'  
+       if adelita_sensual == 1:
+              nfa['N'] = {}
+              nfa['N']['0'] = 'N'  
+              nfa['N']['1'] = 'N'  
+
+
+'''
+nfa = {'A'   : { '1':['A','B'],       '0':['']}, 
+       'B'   : { '1':['A','B'],       '0':['A','B','C']}, 
+       'C'   : { '1':['A','B','D'],   '0':['A','B','C']}, 
+       'D'   : { '1':['A','B'],       '0':['A','B','C']}}
+
+nfa = {'A'   : { '1':['A','B'],       '0':['N']}, 
+       'B'   : { '1':['A','B'],       '0':['A','B','C']}, 
+       'C'   : { '1':['A','B','D'],   '0':['A','B','C']}, 
+       'D'   : { '1':['A','B'],       '0':['A','B','C']}}
+'''
+# TODO: Agregar caso con vacio.
+
 def encontrar_transiciones(estado_inicial):
-       
+
        no_romper = True
 
        while no_romper:
@@ -104,12 +157,28 @@ def encontrar_transiciones(estado_inicial):
 
 
                             # 05. Imprime el estado que se ingresó y a los estados a los que llega con transicion 1 y 0
+
+                            dfa = {}
                             print(f'Transiciones de {sta}:  1:{lista1s}  0:{lista0s}')
+
+                            # TODO: Algoritmo para repetir hasta que no se verifiquen todos _________________________________________
+
+
+                            '''
+                            Output:
+                            dfa = {'A'   : { '1':['BC'],          '0':['E']}, 
+                                   'BC'  : { '1':['AD'],          '0':['E']}, 
+                                   'AD'  : { '1':['BC'],          '0':['AE']}, 
+                                   'E'   : { '1':['N'],           '0':['N']}, 
+                                   'AE'  : { '1':['BC'],          '0':['E']}}
+                            '''
+
+
+
 
 
                             # 06. Agrega los nuevos estados a la lista de estados_afd si no estaba antes
-                            
-                            #     lista1s = 'ABD'                  lista0s = 'ABC'
+                            #     lista1s = 'AB'                  lista0s = 'N'
                             #     estados_afd = ['A','AB','ABC']
                             #     estados_afd = ['A','AB','ABC','ABD']
 
@@ -129,13 +198,6 @@ def encontrar_transiciones(estado_inicial):
 
 
 
-# TODO: Algoritmo para repetir hasta que no se verifiquen todos _________________________________________
-
-
-
-
-
-
 
 
 
@@ -143,6 +205,9 @@ def encontrar_transiciones(estado_inicial):
 #? TEST ________________________________________________________________________________________________
 
 print('\n')
+
+quitar_vacios()
+
 encontrar_transiciones('A')
 #encontrar_transiciones('AB')
 #encontrar_transiciones('ABC')
